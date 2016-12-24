56d32584c4a3633fb5e4cf8f57061a54b66c8009
Jhonatan Córdoba Rodriguez <jhonatanlaur@gmail.com>
2016-12-24 11:20:17 +0100

    Add files via upload

---
+++ gui/partsui/newprj.php
@@ -0,0 +1,23 @@
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Nuevo Proyecto Layer</h4>
+      </div>
+      <div class="modal-body">
+        
+	 <div class="form-group">
+	    <label for="exampleInputEmail1">Nombre Proyecto</label>
+	    <input type="text" class="form-control" id="nameprj" name="nameprj" placeholder="Nombre Proyecto">
+	  </div>
+
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" onclick="crear();">Crear</button>
+      </div>
+    </div>
+  </div>
+</div>
